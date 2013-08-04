64ef3ae90e9915f0f33699a952ab8b72eea005b6
Roger Granda <rxgq@hotmail.com>
2013-08-04 17:26:36 -0500

    Splash Screen

--- gen/com/giandroid/lumi/R.java
+++ gen/com/giandroid/lumi/R.java
@@ -22,12 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int start=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_menu_lumi=0x7f030000;
+        public static final int activity_splash_screen=0x7f030001;
     }
     public static final class menu {
         public static final int menu_lumi=0x7f070000;
@@ -35,7 +38,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int descSplash=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_splash_screen=0x7f050003;
     }
     public static final class style {
         /** 
